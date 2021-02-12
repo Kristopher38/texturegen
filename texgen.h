0e32e56835df243cb3464ed68c9b8bb1de35d3e3
Kristopher38 <kristopher38@wp.pl>
2021-02-12 17:01:29 +0100

    Missing header update in last commit

--- texgen.h
+++ texgen.h
@@ -26,11 +26,19 @@ typedef struct TextureFunc
     Pixel pref_tint;
 } TextureFunc;
 
-Pixel texture_eval_frac(int x, int y, double* slidervals);
+Pixel frac_sum(int x, int y, double freq, double gain, double lacunarity, double (*noise_f)(double, double));
+
+Pixel texture_eval_perlin_noise(int x, int y, double* slidervals);
+
+Pixel texture_eval_value_noise(int x, int y, double* slidervals);
+
+double smoke(double x, double y);
+
+Pixel texture_eval_smoke(int x, int y, double* slidervals);
+
+Pixel texture_eval_clouds(int x, int y, double* slidervals);
 
 Pixel texture_eval_sin(int _x, int _y, double* slidervals);
-
-Pixel texture_eval_empty(int x, int y, double* slidervals);
 
 Pixel texture_eval_gradient(int x, int y, double* slidervals);
 
@@ -41,6 +49,8 @@ double wood(double x, double l);
 double dist(double x, double y);
 
 Pixel texture_eval_wood(int xi, int yi, double* slidervals);
+
+Pixel texture_eval_marble(int x, int y, double* slidervals);
 
 extern TextureFunc tex_funcs[];
 extern gint tex_funcs_n;
